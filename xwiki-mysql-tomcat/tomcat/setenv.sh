a89cfbefc686aa6ecb99e9d14b988ff29f369ac2
Vincent Massol <vincent@massol.net>
2017-01-30 11:20:12 +0100

    XDOCKER-9: Base the XWiki image on tomcat:8-jre8 * Fix typo from previous commit * Remove the need for sudo and the need to execute as tomcat user * Removed CATALINA_HOME and CATALINA_BASE from setenv.sh since they're not taken into account and copy setevn.sh to the right place * Nano wasn't needed either (was a leftover)

--- xwiki-mysql-tomcat/tomcat/setenv.sh
+++ xwiki-mysql-tomcat/tomcat/setenv.sh
@@ -1,3 +1,1 @@
-export CATALINA_HOME=/usr/share/tomcat8
-export CATALINA_BASE=/var/lib/tomcat8
 export CATALINA_OPTS="-Xmx1024m"
--- xwiki-mysql-tomcat/xwiki/start_xwiki.sh
+++ xwiki-mysql-tomcat/xwiki/start_xwiki.sh
@@ -2,13 +2,13 @@
 
 function first_start() {
   configure
-  sudo -u tomcat8 touch /var/lib/tomcat8/.first_start_completed
+  touch /usr/local/xwiki/.first_start_completed
 }
 
 function configure() {
   echo 'Configuring XWiki...'
-  sudo -u tomcat8 sed -i "s/replacemysqluser/${MYSQL_USERNAME:-xwiki}/g" /var/lib/tomcat8/webapps/ROOT/WEB-INF/hibernate.cfg.xml
-  sudo -u tomcat8 sed -i "s/replacemysqlpassword/${MYSQL_PASSWORD:-xwiki}/g" /var/lib/tomcat8/webapps/ROOT/WEB-INF/hibernate.cfg.xml
+  sed -i "s/replacemysqluser/${MYSQL_USERNAME:-xwiki}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
+  sed -i "s/replacemysqlpassword/${MYSQL_PASSWORD:-xwiki}/g" /usr/local/tomcat/webapps/ROOT/WEB-INF/hibernate.cfg.xml
 
   echo '  Using filesystem-based attachments...'
   xwiki-set-cfg 'xwiki.store.attachment.hint' 'file'
@@ -19,13 +19,13 @@ function configure() {
   xwiki-set-cfg 'xwiki.authentication.encryptionKey' "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
 
   echo '  Setting permanent directory...'
-  xwiki-set-properties 'environment.permanentDirectory' '/var/lib/xwiki/data'
+  xwiki-set-properties 'environment.permanentDirectory' '/usr/local/xwiki/data'
   echo '  Configure libreoffice...'
   xwiki-set-properties 'openoffice.autoStart' 'true'
 }
 
-if [[ ! -f /var/lib/tomcat/.first_start_completed ]]; then
+if [[ ! -f /usr/local/xwiki/.first_start_completed ]]; then
   first_start
 fi
 
-sudo -u tomcat8 /usr/share/tomcat8/bin/catalina.sh run
+/usr/local/tomcat/bin/catalina.sh run
