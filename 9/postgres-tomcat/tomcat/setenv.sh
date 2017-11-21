3b47cc3f79fd80bc5af658f82f001da5e55a4878
Vincent Massol <vincent@massol.net>
2017-11-21 09:43:09 +0100

    XDOCKER-38: Configure Tomcat to allow encoded slash '%2F' and backslash '%5C' in URLs * Added explanations

--- 9/postgres-tomcat/tomcat/setenv.sh
+++ 9/postgres-tomcat/tomcat/setenv.sh
@@ -1,1 +1,3 @@
+# By default, Tomcat does not allow the usage of encoded slash '%2F' and backslash '%5C' in URLs, as noted in http://tomcat.apache.org/security-6.html#Fixed_in_Apache_Tomcat_6.0.10.
+# This is why we're passing 2 system properties to allow for them as it's useful to be able to have '/' and '' in wiki pahe names.
 export CATALINA_OPTS="-Xmx1024m -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true"
