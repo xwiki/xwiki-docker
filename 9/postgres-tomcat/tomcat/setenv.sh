3af16d43fc6393b38a1fb177d81d253ed1c9c73e
Vincent Massol <vincent@massol.net>
2018-03-30 13:35:55 +0200

    [Misc] Added missing license headers

--- 9/postgres-tomcat/tomcat/setenv.sh
+++ 9/postgres-tomcat/tomcat/setenv.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# See the NOTICE file distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+# ---------------------------------------------------------------------------
+
 # We're making the following changes to the default:
 # * Adding more memory (default is 512MB which is not enough for XWiki)
 # * By default, Tomcat does not allow the usage of encoded slash '%2F' and backslash '%5C' in URLs, as noted in
