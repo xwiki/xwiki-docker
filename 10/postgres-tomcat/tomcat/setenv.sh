7d5ce3c4e797114e5be7fb1784666899b088b5aa
J0WI <J0WI@users.noreply.github.com>
2019-04-29 13:40:42 +0200

    Update urls to use https

--- 10/postgres-tomcat/tomcat/setenv.sh
+++ 10/postgres-tomcat/tomcat/setenv.sh
@@ -22,7 +22,7 @@
 # We're making the following changes to the default:
 # * Adding more memory (default is 512MB which is not enough for XWiki)
 # * By default, Tomcat does not allow the usage of encoded slash '%2F' and backslash '%5C' in URLs, as noted in
-#   http://tomcat.apache.org/security-6.html#Fixed_in_Apache_Tomcat_6.0.10. We want to allow for them as it's useful to
+#   https://tomcat.apache.org/security-6.html#Fixed_in_Apache_Tomcat_6.0.10. We want to allow for them as it's useful to
 #   be able to have '/' and '' in wiki page names.
 # * On some system /dev/random is slow to init leading to a slow Tomcat and thus Xwiki startup.
 
