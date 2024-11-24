8bb482c068a67d81cbb483d2ecefe455daec5ef1
Artyom Hackimullin <xakimullin2@gmail.com>
2024-11-25 01:07:11 +0300

    SUPPORT-8700: added custom Dockerfile and micord theme extension

--- 16.4/postgres-tomcat/xwiki/docker-entrypoint.sh
+++ 16.4/postgres-tomcat/xwiki/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # ---------------------------------------------------------------------------
 # See the NOTICE file distributed with this work for additional
 # information regarding copyright ownership.
